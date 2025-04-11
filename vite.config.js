ec053b685842b9bff8ac299915d2db891f3873ff
maulik <maulikkamani5@gmail.com>
2025-04-11 12:20:54 +0530

    Update vite.config.js
    add new backend url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://technova-backend-m5dl.onrender.com',
         changeOrigin:true,
         secure: true,
       },
